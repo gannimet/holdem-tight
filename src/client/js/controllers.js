ae2a300f89fd6e2dda32536ce5468ac1fe847078
gannimet <r.wotzlaw@gmail.com>
2015-02-05 17:49:27 +0100

    Basic, bootstrap based table layout

--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -3,7 +3,7 @@
 	var holdemControllers = angular.module('holdemControllers', []);
 
 	holdemControllers.controller('GameCtrl', ['$scope', function($scope) {
-		$scope.message = 'Hello world!';
+		
 	}]);
 
 })(window);
