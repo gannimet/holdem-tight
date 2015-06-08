2d3d95ffaec70849699d6ab21728bb36e381b8a1
gannimet <r.wotzlaw@gmail.com>
2015-06-08 23:45:14 +0200

    tests for player adding in game controller

--- src/client/tests/gameController.spec.js
+++ src/client/tests/gameController.spec.js
@@ -1,7 +1,26 @@
 describe('unit test for game controller', function() {
 	var scope, gameService, uiService, HOLDEM_EVENTS, $modal;
-	var mockGameService = {};
-	var mockUiService = {};
+
+	var mockGameService = {
+		players: [],
+		currentBlinds: {
+			smallBlind: 10,
+			bigBlind: 20
+		},
+		addPlayer: function(player) {
+			this.players.push(player);
+		}
+	};
+
+	var mockUiService = {
+		counter: 1,
+		promptForNewPlayer: function(callback) {
+			callback({
+				name: 'Player ' + this.counter++,
+				stack: 1500
+			});
+		}
+	};
 
 	beforeEach(module('holdemControllers'));
 	beforeEach(module('holdemConstants'));
@@ -30,9 +49,28 @@ describe('unit test for game controller', function() {
 		});
 	}));
 
-	describe('test something', function() {
-		it('should do something', function() {
+	describe('test pre-game assignments', function() {
+		it('should hold correct blind values', function() {
 			expect(scope.gameStarted).toBe(false);
+			expect(scope.currentBlinds).toEqual({
+				smallBlind: 10,
+				bigBlind: 20
+			});
+		});
+
+		it('should assign players to scope', function() {
+			scope.addPlayer();
+			scope.addPlayer();
+
+			expect(scope.players.length).toEqual(2);
+			expect(scope.players[0]).toEqual({
+				name: 'Player 1',
+				stack: 1500
+			});
+			expect(scope.players[1]).toEqual({
+				name: 'Player 2',
+				stack: 1500
+			});
 		});
 	});
 });
