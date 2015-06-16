9ba5c1587968942dc44fe5c824f6d16b75e0a855
gannimet <r.wotzlaw@gmail.com>
2015-06-16 09:55:55 +0200

    Test for add player controller

---
+++ src/client/tests/addPlayerController.spec.js
@@ -0,0 +1,61 @@
+describe('unit test for add player controller', function() {
+	var scope, $modalInstance;
+
+	var mockModalInstanceService = {
+		close: function(player) {},
+
+		dismiss: function(reason) {}
+	};
+
+	beforeEach(module('holdemControllers'));
+	beforeEach(module('ui.bootstrap'));
+
+	beforeEach(module('holdemServices', function($provide) {
+		$provide.value('modalInstance', mockModalInstanceService);
+	}));
+
+	beforeEach(inject(function($injector) {
+		scope = $injector.get('$rootScope').$new();
+		$modalInstance = $injector.get('modalInstance');
+
+		$controller = $injector.get('$controller');
+
+		$controller('AddPlayerController', {
+			$scope: scope,
+			$modalInstance: $modalInstance
+		});
+	}));
+
+	describe('test scope functions', function() {
+		beforeEach(function() {
+			spyOn(mockModalInstanceService, 'close');
+			spyOn(mockModalInstanceService, 'dismiss');
+		});
+
+		it('should digest data on ok click', function() {
+			scope.player = null;
+			scope.ok();
+
+			expect(mockModalInstanceService.close).toHaveBeenCalledWith(null);
+			expect(mockModalInstanceService.close.calls.count()).toEqual(1);
+			mockModalInstanceService.close.calls.reset();
+
+			scope.player = { name: 'Hans', stack: '2000' };
+			scope.ok();
+
+			expect(mockModalInstanceService.close).toHaveBeenCalledWith({
+				name: 'Hans', stack: 2000
+			});
+			expect(mockModalInstanceService.close.calls.count()).toEqual(1);
+			mockModalInstanceService.close.calls.reset();
+		});
+
+		it('should receive cancel message on cancel click', function() {
+			scope.cancel();
+
+			expect(mockModalInstanceService.dismiss).toHaveBeenCalledWith('cancel');
+			expect(mockModalInstanceService.dismiss.calls.count()).toEqual(1);
+			mockModalInstanceService.dismiss.calls.reset();
+		});
+	});
+});
